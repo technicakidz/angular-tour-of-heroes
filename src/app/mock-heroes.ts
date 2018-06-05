1ef9d5b46d4eb12702e44f0df1f232e856e18b38
technicakidz <perfumemarch08@yahoo.co.jp>
2018-06-05 16:55:25 +0900

    add subComponent and input decoder

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'ふたりはプリキュア' },
+  { id: 12, name: 'maxheart' },
+  { id: 13, name: 'Splash Star' },
+  { id: 14, name: 'Yes!プリキュア5' },
+  { id: 15, name: 'Yes!プリキュア5GoGo!' },
+  { id: 16, name: 'flesh プリキュア' },
+  { id: 17, name: 'heart catch プリキュア' },
+  { id: 18, name: 'sweet プリキュア' },
+  { id: 19, name: 'smile プリキュア' },
+  { id: 20, name: 'dokidoki プリキュア' }
+];
